13d94ed1a87e121917eac459a0911780778ba3c3
BuildTools <unconfigured@null.spigotmc.org>
2018-02-06 23:50:32 -0600

    adding few files

--- Assignment2/Factorial.java
+++ Assignment2/Factorial.java
@@ -1,23 +1,24 @@
 public class Factorial {
 
     public static long calculate(long n) {
-        long factorial = 1;
-        if (n == 0 && !(n < 0)) {
-            return factorial;
+        if (n  <  0 || n > 20){
+            System.out.println("ERROR");
+            return n;
+        } else if (n == 0 ) {
+            return 1;
         } else {
-            n--;
-            return Factorial.calculate(n);
+
+            return n * Factorial.calculate(n - 1);
         }
     }
 
     public static void main(String[] args){
 
         if(Factorial.calculate(0) == 1){
-            System.out.println("Factorial.calculate(0) returned 1. Test Fail");
+            System.out.println("Factorial.calculate(0) returned 1. Test Passed");
         }
         if(Factorial.calculate(5) == 120){
-            System.out.println("Factorial.calculate(5) returned 120. Test Fail");
+            System.out.println("Factorial.calculate(5) returned 120. Test Passed");
         }
-        System.out.print(Factorial.calculate(3));
     }
 }
