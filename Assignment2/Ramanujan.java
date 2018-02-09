fcf892c2bb5ff92599d3ce22cbeddd5e96856859
BuildTools <unconfigured@null.spigotmc.org>
2018-02-09 16:07:17 -0600

    updating code

--- Assignment2/Ramanujan.java
+++ Assignment2/Ramanujan.java
@@ -1,17 +1,20 @@
-import java.math.BigDecimal;
 
 public class Ramanujan {
 
-    public getRam(int n){
+    public static double getRam(int input){
 
+        double remOutput = 0;
+
+        for (int i = 1; i <= input; i++ ) {
+            remOutput+=((Factorial.calculate(4*i) * (1103 + (26390 * i))) / (Math.pow( Factorial.calculate(i), 4) * Math.pow(396, (4 * i))));
+        }
+        return ( ((2 * Math.sqrt(2)) / Math.pow( 99, 2)) * remOutput);
     }
 
     public static void main(String[] args){
         int input = Integer.parseInt(args[0]);
-        double gregPi = gregFoum(input) * 4;
-        double percentError = ((Math.PI-gregPi)/Math.PI) * 100;
-
-        System.out.println("Pi according to Gregory series: " + gregPi);
-        System.out.println("This differs from Java's value by " + percentError);
+        double ramPI = Ramanujan.getRam( input );
+        System.out.println("Pi according to Ramanujan series: " + ramPI);
+        System.out.println("This differs from Java's value by " + ((Math.PI-ramPI)/Math.PI) * 100);
     }
 }
