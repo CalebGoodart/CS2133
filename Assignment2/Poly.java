72a4af98000ba5cdc92332bf4c3f0b3a49b084ca
BuildTools <unconfigured@null.spigotmc.org>
2018-02-13 11:05:04 -0600

    transfer

---
+++ Assignment2/Poly.java
@@ -0,0 +1,26 @@
+public class Poly {
+
+    public Poly(int[] coefficients){
+
+    }
+
+    public int degree(){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public Poly add(Poly a){
+
+    }
+
+    public double evaluate(double x){
+
+    }
+
+    public static void main(String[] args){
+          
+    }
+}
