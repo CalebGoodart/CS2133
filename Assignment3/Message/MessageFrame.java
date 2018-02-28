6e9e4f9b2055b18b5be93d3c4b8dac54903d0fee
BuildTools <unconfigured@null.spigotmc.org>
2018-02-28 15:07:49 -0600

    updating code

--- Assignment3/Message/MessageFrame.java
+++ Assignment3/Message/MessageFrame.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MessageFrame extends JFrame{
 
     public MessageFrame() {
-        setTitle("MyFrame");
-        setSize(200,200);
+        setTitle("Message in a bottle");
+        setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new MessagePanel());
         setVisible(true);}
 }
--- Assignment3/Message/MessagePanel.java
+++ Assignment3/Message/MessagePanel.java
@@ -1,4 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Toolkit.getDefaultToolkit;
 
 public class MessagePanel extends JPanel{
+
+    private Toolkit kit = getDefaultToolkit();
+
+    public MessagePanel(){
+        setLayout(new BorderLayout());
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        int CENTERX = kit.getScreenSize().width/2;
+        int CENTERY = kit.getScreenSize().height/2;
+        g.drawString("*imaginary ship*", CENTERX - 30, CENTERY);
+        //g.drawRoundRect(kit.getcreenSize().width/2, kit.getScreenSize().height/4,125, 300, 30, 30);
+        g.drawLine(CENTERX - 30, CENTERY + 150, CENTERX + 80, CENTERY + 150);
+        g.drawArc(CENTERX, CENTERY, 50, 50, 0, 90);
+    }
 }
