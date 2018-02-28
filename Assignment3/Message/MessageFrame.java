3938291758c362f8041dcde693b23bb2ac4e1ff7
BuildTools <unconfigured@null.spigotmc.org>
2018-02-28 11:15:57 -0600

    reorgonizing

---
+++ Assignment3/Message/MessageFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MessageFrame extends JFrame{
+
+    public MessageFrame() {
+        setTitle("MyFrame");
+        setSize(200,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);}
+}
