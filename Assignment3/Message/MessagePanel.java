f37e0a514b5d320b97e3d7bc7c7c048afc936163
BuildTools <unconfigured@null.spigotmc.org>
2018-03-02 02:16:23 -0600

    adding files

--- Assignment3/Message/MessagePanel.java
+++ Assignment3/Message/MessagePanel.java
@@ -18,8 +18,15 @@ public class MessagePanel extends JPanel{
         int CENTERX = kit.getScreenSize().width/2;
         int CENTERY = kit.getScreenSize().height/2;
         g.drawString("*imaginary ship*", CENTERX - 30, CENTERY);
-        //g.drawRoundRect(kit.getcreenSize().width/2, kit.getScreenSize().height/4,125, 300, 30, 30);
         g.drawLine(CENTERX - 30, CENTERY + 150, CENTERX + 80, CENTERY + 150);
-        g.drawArc(CENTERX, CENTERY, 50, 50, 0, 90);
+        g.drawArc(CENTERX + 40, CENTERY  - 25, 75, 175, 270, 90);
+        g.drawArc(CENTERX - 65, CENTERY  - 25, 75, 175, 180, 90);
+        g.drawLine(CENTERX - 65, CENTERY - 100, CENTERX - 65 , CENTERY + 65);
+        g.drawLine(CENTERX + 115, CENTERY - 100, CENTERX + 115, CENTERY + 65);
+        g.drawArc(CENTERX -35, CENTERY  - 250, 150, 300, 0, 90);
+        g.drawArc(CENTERX -65, CENTERY  - 250, 150, 300, 90, 90);
+        g.drawLine(CENTERX + 10, CENTERY - 280, CENTERX + 10, CENTERY - 250);
+        g.drawLine(CENTERX + 40, CENTERY - 280, CENTERX + 40, CENTERY - 250);
+        g.drawLine(CENTERX + 10, CENTERY - 280, CENTERX + 40, CENTERY - 280);
     }
 }
---
+++ Assignment3/Triangles/TrianglePanel.java
@@ -0,0 +1,2 @@
+public class TrianglePanel {
+}
